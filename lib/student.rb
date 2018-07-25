14daae8e8aae3da3885bc6b2072e21fae7a7e9d0
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-25 16:30:21 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -67,6 +67,7 @@ class Student
   def self.count_all_students_in_grade_9
     sql = <<-SQL
       SELECT COUNT(name)
+      FROM students
       WHERE grade = 9 
       SQL
     DB[:conn].execute(sql)
